a84db66f1f08f75c29be70f8ca81758e38e3043d
TaiVo <vtuantai99@gmail.com>
2024-09-10 15:43:39 +0700

    Add navbar for signin page

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent {
-
-}
+export class SignInComponent {}
