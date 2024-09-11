b2d81d8d0775cf8bd570d5fb8c60cac3762eb2ca
TaiVo <vtuantai99@gmail.com>
2024-09-11 12:30:50 +0700

    Done signup page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SignInComponent } from "./sign-in/sign-in.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignInComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
