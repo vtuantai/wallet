26f41478cbac91fb617b313f2ae75a0b5688c2a0
TaiVo <vtuantai99@gmail.com>
2024-09-12 18:40:07 +0700

    done swap language icon EN and VN

--- src/app/language/language.component.ts
+++ src/app/language/language.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-language',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './language.component.html',
   styleUrl: './language.component.css'
 })
 export class LanguageComponent {
+  selectedLanguage: string = 'vn'; // Set default language to "EN"
 
+  switchLanguage(language: string) {
+    this.selectedLanguage = language; // Update selected language
+    //Add additional logic here, like calling a language service or refreshing content
+  }
 }
--- src/app/navbar/nav-main-page/nav-main-page.component.ts
+++ src/app/navbar/nav-main-page/nav-main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LanguageComponent } from "../../language/language.component";
 
 @Component({
   selector: 'app-nav-main-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, LanguageComponent],
   templateUrl: './nav-main-page.component.html',
   styleUrl: './nav-main-page.component.css'
 })
