c5c57ae32c72f70d227ad0bd93320a7014d0e7b0
TaiVo <vtuantai99@gmail.com>
2024-09-17 11:50:42 +0700

    Done navbar for home page

--- src/app/navbar/nav-sign-on/nav-sign-on.component.ts
+++ src/app/navbar/nav-sign-on/nav-sign-on.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-sign-on',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './nav-sign-on.component.html',
   styleUrl: './nav-sign-on.component.css'
 })
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,17 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, HostListener } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-profile',
+  selector: 'app-notification',
   standalone: true,
   imports: [
     CommonModule
   ],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.css'
 })
-export class ProfileComponent {
+export class NotificationComponent {
   isDropdownVisible: boolean = false;
+  unreadCount: number = 3;
+
+  notifications = [
+    { message: 'You have a new message' },
+    { message: 'New friend request' },
+    { message: 'Update available' }
+  ];
 
   constructor(private elementRef: ElementRef) {}
 
@@ -19,10 +27,12 @@ export class ProfileComponent {
     this.isDropdownVisible = !this.isDropdownVisible;
   }
 
+  // Listen for document clicks
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
     const clickedInside = this.elementRef.nativeElement.contains(event.target);
-    if (!clickedInside)
-      this.isDropdownVisible = false;
+    if (!clickedInside) {
+      this.isDropdownVisible = false; // Close the dropdown if the click is outside
+    }
   }
 }
