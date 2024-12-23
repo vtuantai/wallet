d4679b9a1aa0e0099c276d8dd524658a65d0f2f1
TaiVo <vtuantai99@gmail.com>
2024-12-23 14:45:26 +0700

    Done password lists

--- src/app/list/password/password.component.ts
+++ src/app/list/password/password.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './password.component.html',
   styleUrl: './password.component.css'
 })
 export class PasswordComponent {
-
+  items = Array(15).fill({ title: 'webs or apps', subtitle: 'number accounts' });
 }
